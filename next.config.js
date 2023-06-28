0afc803b189f002efaf2bd58fcfb83631ffac25c
SSF17 <72423263+SSF17@users.noreply.github.com>
2023-06-28 01:18:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/app/registro',
+          destination: 'https://ssf-17-github-io.vercel.app/registro', // Reemplaza con la URL de tu servidor
+        },
+      ];
+    },
+  };
